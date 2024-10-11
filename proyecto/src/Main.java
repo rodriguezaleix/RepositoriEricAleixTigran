3e2937cc9675f85cc614cc5103d643374f38e89f
Eric <eric.lorenzo@gracia.lasalle.cat>
2024-10-11 13:08:40 +0200

    Inici variables projecte

---
+++ proyecto/src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        //Inicialització variables
+        int carmels = 0;
+        int nets = 0;
+        int sobrants =0;
+    }
+}
